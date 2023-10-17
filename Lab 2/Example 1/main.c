f535a0b4bd56994d04d0d7ea85f46aaf5a578d41
NikX <89803680+NikXProG@users.noreply.github.com>
2023-10-17 23:34:05 +0300

    All commit file

--- Lab 2/Example 1/main.c
+++ Lab 2/Example 1/main.c
@@ -6,31 +6,37 @@
 #include <malloc.h>
 #include <ctype.h>
 
+enum StatusCode{
+    INVALID_COUNT_ARGUMENTS,
+    NEGATIVE_NUBMERS,
+    DIVIDE_BY_ZERO,
+    INPUT_ERROR
+};
+
 int fuct_strcmp(const char *, const char *);
 char * fuct_memcpy(char *, const char *, size_t);
 int fuct_strlen(const char *);
 char * fuct_reversed(const char *);
 char * fuct_noeven_toupper(const char *);
 char * fuct_sort(const char *);
+char* concatenate_strings(unsigned int num_strings, char* strings[], unsigned int seed);
+
+
+
 
 int main(int argc, char *argv[]){
-    unsigned int char_concatenation;
-    // терминальный нуль + строка состоящая из 2 символов флага
-    if (argc != 3){
-        errno = EINVAL;
-        perror("An invalid number of arguments was entered");
-        return errno;        
-    }
-    /*if (sscanf(argv[1],"%s") != 1){
-        errno = EINVAL;
-        perror("Error: you probably entered something other than an const char * type (enter a string value) ");     
-        return errno;
-    }*/
+    char* result;
+
+
 
     if (fuct_strcmp(argv[1],"-l") == 0){
+        if (argc != 3){
+            perror("Unfortunately, a memory non-allocation error occurred");
+            return 1;        
+        }
         printf("%d",fuct_strlen(argv[2]));
     }
-    if (fuct_strcmp(argv[1],"-r") == 0){
+    else if (fuct_strcmp(argv[1],"-r") == 0){
 
         if (fuct_reversed(argv[2]) == NULL){
             errno = ENOMEM;
@@ -40,7 +46,7 @@ int main(int argc, char *argv[]){
 
         printf("%s",fuct_reversed(argv[2]));        
     }
-    if (fuct_strcmp(argv[1],"-u") == 0){
+    else if (fuct_strcmp(argv[1],"-u") == 0){
 
         if (fuct_noeven_toupper(argv[2]) == NULL){
             errno = ENOMEM;
@@ -50,7 +56,7 @@ int main(int argc, char *argv[]){
     
         printf("%s",fuct_noeven_toupper(argv[2]));            
     }
-    if (fuct_strcmp(argv[1],"-n") == 0){
+    else if (fuct_strcmp(argv[1],"-n") == 0){
         if (fuct_sort(argv[2]) == NULL){
             errno = ENOMEM;
             perror("Unfortunately, a memory non-allocation error occurred");
@@ -58,37 +64,23 @@ int main(int argc, char *argv[]){
         }
         printf("%s",fuct_sort(argv[2]));  
     }
-    
+    else if (fuct_strcmp(argv[1], "-c") == 0) {
+        // Получаем новую строку, являющуюся конкатенацией переданных строк
+        unsigned int seed = atoi(argv[2]);
+        result = concatenate_strings(argc, argv, seed);
+
+        // Выводим результат
+        printf("Concatenated string: %s\n", result);
+
+        // Освобождаем память, выделенную для результата
+        free(result);
+    }    
+    else {
+        printf("Unknown flag: %s\n", argv[1]);
+        return 4;
+    }
     return 0;
-    /*if ( (sscanf(argv[2],"%u",&char_concatenation) || sscanf(argv[1],"%s", char_buffer_array)) != 1) {
-        errno = EIO;
-        perror("Error: you probably entered something other than an Unsigned Int type (enter a value range from 0 to 4,294,967,295) ");     
-        return errno;
-    }
-    else if(sscanf(argv[1],"%s", char_buffer_array) == 1){
 
-    }*/
-
-    /*if (strcmp(argv[1],"-c") == 0){
-
-        if (!((atoll(argv[2]) >= 0) && (atoll(argv[2]) <= UINT_MAX))){
-            errno = ERANGE;
-            perror("The entered number is not in the range unsigned __int32 (Unsigned int)");
-            return errno;
-        }
-
-        if (sscanf(argv[2],"%u",&char_concatenation) != 1){
-            errno = EINVAL;
-            perror("Error: you probably entered something other than an Unsigned Int type (enter a value range from 0 to 4,294,967,295) ");     
-            return errno;
-        }
-
-    }
-    if (sscanf(argv[2],"%s", char_buffer_array) == 1){
-        errno = EINVAL;
-        perror("Error: you probably entered something other than an const char * type (enter a string value) ");     
-        return errno;
-    }*/
 }
 
 int fuct_strcmp(const char * argv, const char * flag){
@@ -104,12 +96,16 @@ int fuct_strcmp(const char * argv, const char * flag){
     }
     return check_flag ? 0 : 1;
 }
-char * fuct_memcpy(char* destination, const char * source, size_t n){
-    for (size_t i = 0; i < n; i++) {
-        destination[i] = source[i];
+
+void func_strcpy(char *destination, const char *sourse) {
+    while (*sourse) {
+        *destination = *sourse;
+        destination++;
+        sourse++;
     }
-    return destination;
+    *destination = '\0';
 }
+
 
 int fuct_strlen(const char * string){
     int i, count_char = 0;
@@ -158,30 +154,69 @@ char * fuct_noeven_toupper(const char * string){
 
 char * fuct_sort(const char * string){
     int i, strlen = (fuct_strlen(string) + 1);
-    char * digits  = (char *) malloc( strlen * sizeof(char) ); 
-    char * letters =  (char *) malloc( strlen * sizeof(char) ); 
-    char * symbols =  (char *) malloc( strlen * sizeof(char) ); 
     char * array_string = (char *) malloc( strlen * sizeof(char) );  
+    int count = 0;
 
     for(i=0;i<strlen-1; i++){
         if (isdigit(string[i])){
-            digits[i] = string[i];
+            array_string[count++] = string[i];
         }
-        else if (isalpha(string[i])){
-            letters[i] = string[i];
-        }
-        else{
-            symbols[i] = string[i];
-        }
-
     }
-    int size1 = (fuct_strlen(digits) + 1);
-    int size2 = (fuct_strlen(letters) + 1);
-    int size3 = (fuct_strlen(symbols) + 1);
-    printf("%d %d %d\n",size1,size2,size3);
-    fuct_memcpy(array_string,  digits , strlen * sizeof(char) );
-    fuct_memcpy(array_string + strlen,  letters , strlen * sizeof(char) );
-    fuct_memcpy(array_string + 2*strlen,  symbols , strlen * sizeof(char) );   
-    //memcpy(array_string + 2 * strlen, symbols, strlen * sizeof(int));
+
+    for(i=0;i<strlen-1; i++){
+        if (isalpha(string[i])){
+            array_string[count++] = string[i];
+        }
+    }
+    
+    for(i=0;i<strlen-1; i++){
+        if ( !(isalpha(string[i])) && !(isdigit(string[i]))  ){
+            array_string[count++] = string[i];
+        }
+    }
+
+    array_string[count] = '\0';
+
     return array_string;
+}
+char* concatenate_strings(unsigned int num_strings, char* strings[], unsigned int seed) {
+    srand(seed);
+
+    // Создаем массив для хранения индексов строк
+    unsigned int* indices = (unsigned int*)malloc(num_strings * sizeof(unsigned int));
+    for (unsigned int i = 0; i < num_strings; ++i) {
+        indices[i] = i;
+    }
+
+    // Перемешиваем индексы в массиве
+    for (unsigned int i = num_strings - 1; i > 0; --i) {
+        unsigned int j = rand() % (i + 1);
+        // Меняем значения местами
+        unsigned int temp = indices[i];
+        indices[i] = indices[j];
+        indices[j] = temp;
+    }
+
+    // Вычисляем общую длину конкатенированных строк
+    unsigned int total_length = 0;
+    for (unsigned int i = 0; i < num_strings; ++i) {
+        total_length += fuct_strlen(strings[indices[i]]);
+    }
+
+    // Выделяем память под результат
+    char* result = (char*)malloc((total_length + 1) * sizeof(char));
+
+    // Копируем строки в результирующую строку
+    unsigned int current_position = 0;
+    for (unsigned int i = 0; i < num_strings; ++i) {
+        unsigned int index = indices[i];
+        unsigned int length = fuct_strlen(strings[index]);
+        func_strcpy(result + current_position, strings[index]);
+        current_position += length;
+    }
+
+    // Освобождаем память, выделенную для индексов
+    free(indices);
+
+    return result;
 }
--- Lab 2/Example 3/main.c
+++ Lab 2/Example 3/main.c
@@ -1,26 +1,76 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdarg.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 
-double geometric_mean(int count, ...) {
-    va_list args;
-    double product = 1.0;
+enum StatusCode{
+    INCORRECTED_READ_FILE
+};
 
-    va_start(args, count);
-    for (int i = 0; i < count; i++) {
-        double num = va_arg(args, double);
-        product *= num;
+enum StatusCode find_substring_in_files(const char *substring, int num_files, ...);
+
+int main()
+{
+    enum StatusCode Status = find_substring_in_files("example", 2, "file1.txt", "file2.txt");
+    if (Status == INCORRECTED_READ_FILE){
+        printf("Incorrected Error: invalid read file");
+        return 1;
     }
-    va_end(args);
-
-    return pow(product, 1.0 / count);
+    return 0;
 }
 
-int main() {
+enum StatusCode find_substring_in_files(const char *substring, int num_files, ...)
+{
+    va_list files;
+    va_start(files, num_files);
 
-    double result = geometric_mean(4, 2.0, 4.0, 8.0, 2.0);
-    printf("%f\n", result); // Выведет результат вычисления среднего геометрического
+    for (int i = 0; i < num_files; i++) {
+        const char *file_path = va_arg(files, const char *);
 
-    return 0;
+        FILE *file;
+        if ( (file = fopen(file_path, "r")) == NULL) {
+            return INCORRECTED_READ_FILE;
+        }
+
+        printf("Searching for \"%s\" in file: %s\n", substring, file_path);
+
+        char line[256];
+        int line_number = 1;
+
+        while (fgets(line, sizeof(line), file)) {
+            int line_length = strlen(line);
+            int column_number = 1;
+            int substring_length = strlen(substring);
+            
+            for (int j = 0; j <= line_length - substring_length; j++) {
+
+                int match = 1;
+
+
+                for (int k = 0; k < substring_length; k++) {
+
+                    if (line[j + k] != substring[k]) {
+                        match = 0;
+                        break;
+                    }
+                }
+                
+
+
+                if (match) {
+                    printf("Found at line: %d, column: %d\n", line_number, column_number);
+                }
+                
+
+
+                column_number++;
+            }
+
+            line_number++;
+        }
+
+        fclose(file);
+    }
+
+    va_end(files);
 }
